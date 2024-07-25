ccf26002e47d8d2ab941040dad65de157c852c0d
Anupriya-ladha <anupriya.ladha@indusnet.co.in>
2024-07-25 05:23:06 -0700

    first commit

---
+++ file.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+fs.writeFileSync("./test.txt", "heplo world"); 
+fs.readFileSync("./text.txt" ,"")
---
+++ math.js
@@ -0,0 +1,12 @@
+function product(a,b){
+    return a*b;
+}
+function add(a,b)
+{
+    return a+b;
+}
+function sub(a,b)
+{
+    return a-b;
+}
+module.exports={ product,add,sub};
